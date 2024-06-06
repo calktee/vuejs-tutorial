2df1cdf3f62369f7680334159419077aa1f538b5
calktee <137979909+calktee@users.noreply.github.com>
2024-06-07 05:22:53 +0900

    refactor:Add todo-app folder to store other apps

---
+++ todo-app/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="wrap">
+    <div>Header</div>
+    <main class="main">Main</main>
+    <div>Footer</div>
+  </div>
+</template>
+
+<style>
+@import 'assets/css/destyle.css';
+
+.wrap {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  min-height: 100vh;
+  width: 370px;
+  margin: 0px;
+  font-family: sans-serif;
+}
+
+.main {
+  flex: 1;
+  width: 100%;
+}
+</style>
